4ce428a2fd4333a80e9d6a8d106fa17559e262bd
bryan <bohorquez.brayan@correunivalle.edu.co>
2026-03-10 14:22:31 -0500

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === "serve" ? "/" : "/kawsay-Pisinascas/",
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
+export default defineConfig({
+  base: "/kawsay-PiscinasYjacuzzis/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
